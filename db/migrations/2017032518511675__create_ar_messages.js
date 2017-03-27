108cf13051b44027f31571fe3545379bca2dcef3
Eric Periou <eperiou@yahoo.com>
2017-03-27 15:26:29 -0500

    (feature) Add seed data for ArMessages, edit schema to reflect changes `

--- db/migrations/2017032518511675__create_ar_messages.js
+++ db/migrations/2017032518511675__create_ar_messages.js
@@ -13,6 +13,7 @@ class CreateArMessages extends Nodal.Migration {
     return [
       this.createTable('ar_messages',
         [{ name: 'username', type: 'string' },
+        { name: 'message', type: 'text' },
       { name: 'bearing', type: 'int' },
       { name: 'crumb_id', type: 'int' }]),
     ];
