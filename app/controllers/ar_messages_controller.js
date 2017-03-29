051e0dc26c5df8249735a8ce5268e008a3959692
Eric Periou <eperiou@yahoo.com>
2017-03-29 16:32:32 -0500

    (feature) Promisify ArMessage controller

--- app/controllers/ar_messages_controller.js
+++ app/controllers/ar_messages_controller.js
@@ -4,7 +4,7 @@ const Nodal = require('nodal');
 
 const ArMessage = Nodal.require('app/models/ar_message.js');
 const AuthController = Nodal.require('app/controllers/auth_controller.js');
-
+const Q = require('q');
 
 class ArMessagesController extends AuthController {
 
@@ -22,33 +22,37 @@ class ArMessagesController extends AuthController {
     this.authorize((accessToken, user) => {
       this.params.body.user_id = user.get('id');
     });
-    ArMessage.find(this.params.route.id, (err, model) => {
-      this.respond(err || model);
-    });
+    const findArMessage = Q.nbind(ArMessage.find, ArMessage);
+    findArMessage(this.params.route.id)
+      .then((model) => { this.respond(model); })
+      .catch((err) => { this.respond(err); });
   }
   create() {
     this.authorize((accessToken, user) => {
       this.params.body.user_id = user.get('id');
     });
-    ArMessage.create(this.params.body, (err, model) => {
-      this.respond(err || model);
-    });
+    const createArMessage = Q.nbind(ArMessage.create, ArMessage);
+    createArMessage(this.params.body)
+      .then((model) => { this.respond(model); })
+      .catch((err) => { this.respond(err); });
   }
   update() {
     this.authorize((accessToken, user) => {
       this.params.body.user_id = user.get('id');
     });
-    ArMessage.update(this.params.route.id, this.params.body, (err, model) => {
-      this.respond(err || model);
-    });
+    const updateArMessage = Q.nbind(ArMessage.update, ArMessage);
+    updateArMessage(this.params.route.id, this.params.body)
+      .then((model) => { this.respond(model); })
+      .catch((err) => { this.respond(err); });
   }
   destroy() {
     this.authorize((accessToken, user) => {
       this.params.body.user_id = user.get('id');
     });
-    ArMessage.destroy(this.params.route.id, (err, model) => {
-      this.respond(err || model);
-    });
+    const deleteArMessage = Q.nbind(ArMessage.destroy, ArMessage);
+    deleteArMessage(this.params.route.id)
+      .then(() => { this.respond('ArMessage has been deleted'); })
+      .catch((err) => { this.respond(err); });
   }
 }
 
