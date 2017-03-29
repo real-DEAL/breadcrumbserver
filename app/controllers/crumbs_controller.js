720c263cbfbd5484fd9a5cea7be328bfe65c5268
Eric Periou <eperiou@yahoo.com>
2017-03-29 13:54:11 -0500

    (feature) Promisify the crumb controller and checked auth checker function

--- app/controllers/crumbs_controller.js
+++ app/controllers/crumbs_controller.js
@@ -4,6 +4,8 @@ const Nodal = require('nodal');
 
 const Crumb = Nodal.require('app/models/crumb.js');
 const AuthController = Nodal.require('app/controllers/auth_controller.js');
+const Q = require('q');
+
 class CrumbsController extends AuthController {
   index() {
     this.authorize((accessToken, user) => {
@@ -19,17 +21,19 @@ class CrumbsController extends AuthController {
     this.authorize((accessToken, user) => {
       this.params.body.user_id = user.get('id');
     });
-    Crumb.find(this.params.route.id, (err, model) => {
-      this.respond(err || model);
-    });
+    const findCrumb = Q.nbind(Crumb.find, Crumb);
+    findCrumb(this.params.route.id)
+      .then((model) => { this.respond(model); })
+      .catch((err) => { this.respond(err); });
   }
   create() {
     this.authorize((accessToken, user) => {
       this.params.body.user_id = user.get('id');
     });
-    Crumb.create(this.params.body, (err, model) => {
-      this.respond(err || model);
-    });
+    const createCrumb = Q.nbind(Crumb.create, Crumb);
+    createCrumb(this.params.body)
+      .then((model) => { this.respond(model); })
+      .catch((err) => { this.respond(err); });
   }
   update() {
     this.authorize((accessToken, user) => {
@@ -38,14 +42,23 @@ class CrumbsController extends AuthController {
     Crumb.update(this.params.route.id, this.params.body, (err, model) => {
       this.respond(err || model);
     });
+
+    // TODO currently not using this route, will need to test when we do
+    // have already instantiated the possible promise pattern
+    // const updateCrumb = Q.nbind(Crumb.update, Crumb);
+    // updateCrumb(this.params.route.id, this.params.body)
+    //   .then((model) => { this.respond(model); })
+    //   .catch((err) => { this.respond(err); });
+    //
   }
   destroy() {
     this.authorize((accessToken, user) => {
       this.params.body.user_id = user.get('id');
     });
-    Crumb.destroy(this.params.route.id, (err) => {
-      this.respond(err || 'Crumb has been deleted');
-    });
+    const deleteCrumb = Q.nbind(Crumb.destroy, Crumb);
+    deleteCrumb(this.params.route.id)
+      .then(() => { this.respond('Crumb has been deleted'); })
+      .catch((err) => { this.respond(err); });
   }
 }
 
